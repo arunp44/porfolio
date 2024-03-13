d24f12aa4c7d41440f3af7f729eaa76308fcafc4
arunp44 <137665388+arunp44@users.noreply.github.com>
2024-03-13 11:35:21 -0600

    porfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/footer/Footer.jsx';
+import Header from './components/header/Header.jsx';
+import About from './components/about/About.jsx';
+import Contacts from './components/contacts/Contacts.jsx';
+import Experience from './components/experience/Experience.jsx';
+import Nav from './components/nav/Nav.jsx';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <Nav />
+        <div className="container">
+          <Router>
+            <Routes>
+              <Route path = "/" element = {<Experience/>}> </Route>
+              <Route path = "/about" element = {<About/>}> </Route>
+              <Route path = "/contacts" element = {<Contacts/>}> </Route>
+            </Routes>
+          </Router>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
